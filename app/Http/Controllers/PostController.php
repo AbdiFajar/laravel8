239ecb229b400fd4bfc88bc2db244bf83594729f
AbdiFajar <abdifajarilahi4545@gmail.com>
2023-09-20 16:45:52 +0700

    searchpagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,23 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 
 class PostController extends Controller
 {
     public function index()
-    {
-        $posts = Post::latest();
+    { 
+        $title= '';
+        if(request('category')) {
+            $category = Category::firstWhere('slug', request('category'));
+            $title = ' in ' . $category->name;
+        }
 
-        if(request('search')) {
-            $posts->where('title', 'like', '%' . request('search') . '&')
-                  ->orWhere('body', 'like', '%' . request('search') . '%');
+        if(request('author')) {
+            $author = User::firstWhere('username', request('author'));
+            $title = 'by' . $author->name;
         }
         
         return view('posts', [
-            "title" => "All Posts",
+            "title" => "All Posts" . $title,
             "active" => 'posts',
-            "posts" =>  $posts->get()
+            "posts" =>  Post::latest()->filter(request(['search', 'category', 'author']))->paginate(7)->withQueryString()
         ]);
     }
     
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -8,7 +8,7 @@
         <div class="col-md-8">
 
           <h2>{{$post->title}}</h2>
-          <P>By. <a href="/authors/{{$post->author->username}}"class="text-decoration-none">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}"class="text-decoration-none">{{$post->category->name}}</a></P>
+          <P>By. <a href="/posts?author={{$post->author->username}}"class="text-decoration-none">{{$post->author->name}}</a> in <a href="/posts?category={{$post->category->slug}}"class="text-decoration-none">{{$post->category->name}}</a></P>
             
           <img src="https://source.unsplash.com/1200x500? {{$post->category->name}}" alt="{{$post->category->name}}" class="img-fluid">
 
